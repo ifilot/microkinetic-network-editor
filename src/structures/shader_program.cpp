edf3f07fcfbf15549e589197e354424e2abef815
ifilot <ivo@ivofilot.nl>
2026-03-18 09:39:30 +0100

    Polishing comments

--- src/structures/shader_program.cpp
+++ src/structures/shader_program.cpp
@@ -1,6 +1,23 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-// SlabRender
-// Author: Ivo Filot <ivo@ivofilot.nl>
+/**************************************************************************
+ *   This file is part of MICROKINETIC NETWORK EDITOR.                    *
+ *                                                                        *
+ *   Author: Ivo Filot <ivo@ivofilot.nl>                                  *
+ *                                                                        *
+ *   MICROKINETIC NETWORK EDITOR (MNE) is free software:                  *
+ *   you can redistribute it and/or modify it under the terms of the      *
+ *   GNU General Public License as published by the Free Software         *
+ *   Foundation, either version 3 of the License, or (at your option)     *
+ *   any later version.                                                   *
+ *                                                                        *
+ *   MNE is distributed in the hope that it will be useful,               *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty          *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              *
+ *   See the GNU General Public License for more details.                 *
+ *                                                                        *
+ *   You should have received a copy of the GNU General Public License    *
+ *   along with this program.  If not, see http://www.gnu.org/licenses/.  *
+ *                                                                        *
+ **************************************************************************/
 
 
 #include "shader_program.h"
